f76988a493207f4dbc420a04faac48653df7813c
dsusmit63 <dsusmit63@gmail.com>
2026-05-09 10:00:23 +0530

    added remote backends

---
+++ remote-backends/remote-backends.tf
@@ -0,0 +1,18 @@
+# S3 Bucket to handle state file
+resource "aws_s3_bucket" "myremotes3bucket" {
+  bucket = "myremotes3bucket09052026"
+  tags = {
+    Name = "mys3bucket09052026"
+  }
+}
+
+# Dynamo DB
+resource "aws_dynamodb_table" "myremotedynamodbtable" {
+  name = "myremotedynamodbtable090526"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
