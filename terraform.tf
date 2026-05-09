b0c558bfa61be1deacd25f7319cdea09aa63452c
dsusmit63 <dsusmit63@gmail.com>
2026-05-09 10:01:00 +0530

    added remote backend configurations

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "6.41.0"
     }
   }
+  backend "s3" {
+    bucket = "myremotes3bucket09052026"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "myremotedynamodbtable090526"
+  }
 }
